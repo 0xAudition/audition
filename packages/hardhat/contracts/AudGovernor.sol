5ef4d52ead9cfeadac459c6335042d25c7408794
Sri <2161280+keepRunning@users.noreply.github.com>
2022-05-13 19:55:15 +0530

    Update AudGovernor.sol
    Have kept the voting period as 10 min suitable for Rinkeyby. We need longer for localhost and need to figure suitable time for production.
    No timelock for now.

--- packages/hardhat/contracts/AudGovernor.sol
+++ packages/hardhat/contracts/AudGovernor.sol
@@ -1,8 +1,55 @@
-// pragma solidity ^0.8.0;
-//
-// import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-//
-// contract AudGoverner is GovernorVotes, Ownable {
-//
-// }
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+
+contract AUDGoverner is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    constructor(IVotes _token)
+        Governor("AUDGoverner")
+        GovernorSettings(0 /* 0 block */, 45 /* 10 minutes */, 0)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(2)
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
+
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+}
