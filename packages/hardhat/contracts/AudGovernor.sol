9d67b8a15a4633658918f5f17c28705443474e82
ask03 <aykim043@gmail.com>
2022-05-19 13:33:55 -0700

    remove ivotes interface

--- packages/hardhat/contracts/AudGovernor.sol
+++ packages/hardhat/contracts/AudGovernor.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import "@openzeppelin/contracts/governance/utils/IVotes.sol";
 
 contract AudGoverner is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
     constructor(IVotes _token)
